a3c988114ed7535402e0b9eb4b50340c334308b5
kimhyeok <kimmang940715@naver.com>
2024-05-05 02:04:21 +0900

    feat : 25x25 맵 하드코딩

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -6,13 +6,37 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+            int lastTick = 0;
             while (true)
             {
+                #region 프레임관리
+                // FPS 프레임 60프레임 OK 30프레임이하 NO)
+                int currentTick = System.Environment.TickCount;
+                // 1초는 1000ms
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+
+                lastTick = currentTick;
+                #endregion
                 // 입력
+
                 // 로직
+
                 // 렌더링
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
             }
-            Console.WriteLine("Hello, World!");
         }
     }
 }
