98cd4f9b2ce7e22bf86819887b35477b00e58e37
kimhyeok <kimmang940715@naver.com>
2024-05-06 05:40:31 +0900

    feat : Board 클래스추가

---
+++ Algorithm/Board.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Board
+    {
+        public int[] _data = new int[25];
+        public List<int> _data2 = new List<int>();
+        public LinkedList<int> _data3 = new LinkedList<int>();
+        public void Initialize()
+        {
+
+        }
+    }
+}
